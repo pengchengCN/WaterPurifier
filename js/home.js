5a523f6ba37a1ecad8f99439e07fc69864fefd95
peng0716 <284393960@qq.com>
2018-03-22 10:02:51 +0800

    Site updated: 2018-03-22 10:02:51

--- js/home.js
+++ js/home.js
@@ -17,3 +17,11 @@ function scrollHandler(e) {
 	     navbar.classList.remove("fixed");
 	 }
 }
+// $(document).ready(function(){
+// 	let whdef = 100 / 1920 // 表示1920的设计图,使用100PX的默认值
+// 	// let wH = window.innerHeight // 当前窗口的高度
+// 	let wW = window.innerWidth // 当前窗口的宽度
+// 	let rem = wW * whdef // 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值
+// 	let html = document.getElementsByTagName('html')[0]
+// 	html.style.fontSize = rem + 'px'
+// }) 
